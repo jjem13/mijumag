d504c314eee03f6369eaf19c0748d17922ae48ba
magni <133161506+jjem13@users.noreply.github.com>
2024-10-03 12:17:08 -0300

    j

--- main.js
+++ main.js
@@ -26,9 +26,9 @@ console.log (i);
 console.log(estados[i]);
 i++;
 }
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
+for (let index = 0; index< estados.length; index++) {
+    console.log(index);
+    console.log (estados[index]);
 }
     
 }
