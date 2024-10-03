bd841bda187c0a1494c3a308d77c54ea0fa8ec11
magni <133161506+jjem13@users.noreply.github.com>
2024-10-03 11:24:45 -0300

    asd

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
-let numero=parseInt(prompt('numero'));
-document.write(numero)
-function soma (num1,num2){
-    return num1+num2;
-}
-console.log(soma(1,2));
+// let numero=parseInt(prompt('numero'));
+// document.write(numero)
+// function soma (num1,num2){
+//     return num1+num2;
+// }
+// console.log(soma(1,2));
 
-function areaQuadrado (lado){
-    return lado**2;
-}
-console.log(areaQuadrado(numero));
+// function areaQuadrado (lado){
+//     return lado**2;
+// }
+// console.log(areaQuadrado(numero));
 
-let num1 = parseInt(prompt('Qual numero?'))
-let num2 = parseInt(prompt('Qual numero?'))
-let num3 = parseInt(prompt('Qual numero?'))
+// let num1 = parseInt(prompt('Qual numero?'))
+// let num2 = parseInt(prompt('Qual numero?'))
+// let num3 = parseInt(prompt('Qual numero?'))
 
-function media (x,y,z){
-    return (x+y+z)/3;
-}
-document.write(media(num1, num2, num3))
+// function media (x,y,z){
+//     return (x+y+z)/3;
+// }
+// document.write(media(num1, num2, num3))
+
+let estados=['pr','rj','ac','al','ap','am','pa','ce','df','es','go','ma','mt','ms','mg','pa','pb','pe','pi','rn','rs','ro','rr','sc','sp','se','to']
