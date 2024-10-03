95888e2eeab87e509b53f0b3710b49a6004df7fd
magni <133161506+jjem13@users.noreply.github.com>
2024-10-03 11:19:08 -0300

    y

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-alert('Miguel e Julia')
+let numero=parseInt(prompt('numero'));
+document.write(numero)
+function soma (num1,num2){
+    return num1+num2;
+}
+console.log(soma(1,2));
+
+function areaQuadrado (lado){
+    return lado**2;
+}
+console.log(areaQuadrado(numero));
+
+let num1 = parseInt(prompt('Qual numero?'))
+let num2 = parseInt(prompt('Qual numero?'))
+let num3 = parseInt(prompt('Qual numero?'))
+
+function media (x,y,z){
+    return (x+y+z)/3;
+}
+document.write(media(num1, num2, num3))
